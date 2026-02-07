2cef2fc3f9446d57eaf21bb6e7e30760a1e1ab91
Muhammad hashim <mh4165209@gmail.com>
2026-02-07 20:26:56 +0500

    add new file in practice 3 in JS

---
+++ practice3.js
@@ -0,0 +1,5 @@
+var number1 = prompt ("enter your number");
+number1 = Number (number1);
+var big =  number1 > 10;
+var small = number1 < 16;
+alert ("your number is big " + (big) + " your number is small " + (small));
